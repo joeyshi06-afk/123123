6f5c529f27e706f9d360fb278f614cde4ab2ec7b
joeyshi06-afk <joeyshi06@gmail.com>
2025-12-22 20:45:58 +0800

    Update Experience.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -255,7 +255,7 @@ const SceneContent: React.FC<ExperienceProps> = ({ mixFactor, colors, inputRef, 
       <pointLight position={[0, 10, 10]} intensity={0.5} color="#ffffff" />
       
       <Environment 
-        files='public/hdri/potsdamer_platz_1k.hdr'
+        files='/hdri/potsdamer_platz_1k.hdr'
         background={false} 
       />
       <Stars radius={100} depth={50} count={3000} factor={4} saturation={0} fade speed={1} />
