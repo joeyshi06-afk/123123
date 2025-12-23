1acb5c4f3d90be18782a50e6279e1219c175e5e2
joeyshi06-afk <joeyshi06@gmail.com>
2025-12-23 21:07:22 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -280,28 +280,6 @@ const App: React.FC = () => {
               )}
           </button>
 
-          {/* 2. Upload Photos */}
-          <button 
-            onClick={handleUploadClick}
-            className={iconButtonClass}
-            title="上传照片"
-          >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="currentColor" className="w-5 h-5 md:w-6 md:h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-              </svg>
-          </button>
-
-          {/* 3. Polaroid Signature */}
-          <button 
-            onClick={handleSignatureClick}
-            className={iconButtonClass}
-            title="拍立得签名"
-          >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="currentColor" className="w-5 h-5 md:w-6 md:h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-              </svg>
-          </button>
-
           {/* 4. Disperse/Assemble Toggle */}
           <button 
             onClick={toggleState}
